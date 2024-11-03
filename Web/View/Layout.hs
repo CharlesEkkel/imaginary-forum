792dbe299a278691f47fe301eb8d09b7e7c4c565
Charles Taylor <taylorc822@gmail.com>
2024-11-03 14:11:18 +1100

    chore: remove jquery

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -19,7 +19,7 @@ defaultLayout inner =
                 {stylesheets}
                 {scripts}
 
-                <title>{pageTitleOrDefault "App"}</title>
+                <title>{pageTitleOrDefault "AI Forum"}</title>
             </head>
             <body>
                 <div class="container mt-4">
@@ -46,7 +46,6 @@ scripts :: Html
 scripts =
     [hsx|
         {when isDevelopment devScripts}
-        <script src={assetPath "/vendor/jquery-3.6.0.slim.min.js"}></script>
         <script src={assetPath "/vendor/timeago.js"}></script>
         <script src={assetPath "/vendor/popper-2.11.6.min.js"}></script>
         <script src={assetPath "/vendor/bootstrap-5.2.1/bootstrap.min.js"}></script>
--- static/app.js
+++ static/app.js
@@ -1,3 +1,3 @@
-$(document).on('ready turbolinks:load', function () {
+document.addEventListener('ready turbolinks:load', function() {
     // This is called on the first page load *and* also when the page is changed by turbolinks
 });
